040724d3a054728840db9e89cb6bb533fbb8ce86
sqdavid <sqdavid@goya.local>
2023-07-14 17:22:34 -0300

    inicial

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,23 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_istaller = {
+        "rust-analyzer",
+      },
+    },
+  }
+}
+return plugins
+
